e170149cd48f3f977462377327d592b7b0ebf52e
corytodd <corytodd@users.noreply.github.com>
2018-12-09 01:14:27 -0700

    Add Windows daemon
    Deferred task now runs as a child process. Kind of shitty but... it works so maybe I'll come back and improve it.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,67 @@
 use std::env;
 use std::process;
+use std::process::Command;
 use std::thread;
 use std::time::Duration;
 
-fn main() {
+#[cfg(windows)] extern crate winapi;
+use std::io::Error;
 
+fn main() {
+    let args = env::args().collect::<Vec<_>>();
+
+    // Need at least 3 args: self [in|daemon] [p1 p2 ... pn]
+    if args.len() < 3 {
+        usage(1);
+    }
+
+    match args[1].as_ref() {
+        "daemon" => run_daemon(args),
+        "in" => make_reminder(args),
+        "help" => usage(0),
+        _ => usage(1),
+    }
+}
+
+// Executes daemon function after waiting seconds_delay
+fn run_daemon(args: Vec<String>) {
+
+    println!("Received daemon command {:?}", args);
+
+    // No delay period specified
+    if args.len() != 3{
+        process::exit(1)
+    }
+
+    match args[2].parse::<u64>() {
+        Ok(n) => {
+
+            println!("Daemon received delay of {:?} seconds", n);
+
+            let duration = Duration::new(n, 0);
+            thread::sleep(duration);
+
+            // TODO can I get a real beep? Found beep lib, maybe that.
+            let _result = print_message("Reminder, something something");
+
+            process::exit(0);
+        }
+        Err(_e) => process::exit(1)
+    }
+}
+
+fn make_reminder(args: Vec<String>) {
     // Accumulate parsed time components as seconds
     let mut remind_seconds = 0;
 
-    for arg in env::args().skip(1){
-        match arg[0..arg.len()-1].parse::<u64>() {
+    if args.len() < 2 {
+        return
+    }
+
+    for arg in &args[2..] {
+        match arg[..arg.len() - 1].parse::<u64>() {
             Ok(duration) => {
-                match arg[arg.len()-1..arg.len()].as_ref() {
+                match arg[arg.len() - 1..arg.len()].as_ref() {
                     "s" => remind_seconds += duration,
                     "m" => remind_seconds += duration * 60,
                     "h" => remind_seconds += duration * 60 * 60,
@@ -27,25 +77,48 @@ fn main() {
             }
         }
     }
-    println!("Reminder set for {} seconds", remind_seconds);
 
-    let duration = Duration::new(remind_seconds, 0);
-    thread::sleep(duration);
-    // TODO how to daemonize a process? I don't want to leave the console hanging here...
+    if let Some(command) = args.get(0) {
+        let child = Command::new(command)
+            .arg("daemon")
+            .arg(remind_seconds.to_string())
+            .spawn()
+            .unwrap();
 
-    // TODO can I get a real beep? Found beep lib, maybe that.
-    println!("Ding!");
+        println!("Reminder set for {} seconds on child process {}", remind_seconds, child.id());
+    }
 }
 
 // Show usage string, optionally exit if exit_code is non-zero
 fn usage(exit_code: i32) {
-    println!("remindme [duration][unit]");
-    println!("\n");
+    println!("\nremindme in [duration][unit] ...");
+    println!("[args]");
+    println!("\thelp\t\tShow this message");
     println!("\tduration\t\tInteger duration");
-    println!("\tunit\t\tTime unit character from [s]econds, [m]inutes, or [h]ours");
-    println!("Example: Set a reminder for 1 hour, 30 minutes, and 20 seconds");
-    println!("\tremindme 1h 30m 20s");
-    if exit_code > 0{
+    println!("\tunit\t\t\tTime unit character from [s]econds, [m]inutes, or [h]ours");
+    println!("[example] :: Set a reminder for 1 hour, 30 minutes, and 20 seconds");
+    println!("\tremindme in 1h 30m 20s");
+    if exit_code > 0 {
         process::exit(exit_code)
     }
+}
+
+#[cfg(windows)]
+fn print_message(msg: &str) -> Result<i32, Error> {
+    use std::ffi::OsStr;
+    use std::iter::once;
+    use std::os::windows::ffi::OsStrExt;
+    use std::ptr::null_mut;
+    use winapi::um::winuser::{MB_OK, MessageBoxW};
+    let wide: Vec<u16> = OsStr::new(msg).encode_wide().chain(once(0)).collect();
+    let ret = unsafe {
+        MessageBoxW(null_mut(), wide.as_ptr(), wide.as_ptr(), MB_OK)
+    };
+    if ret == 0 { Err(Error::last_os_error()) }
+        else { Ok(ret) }
+}
+#[cfg(not(windows))]
+fn print_message(msg: &str) -> Result<(), Error> {
+    println!("{}", msg);
+    Ok(())
 }
